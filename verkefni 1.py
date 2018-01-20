c4e4b4733555c03c69f68a49429930f260db72e1
User218 <alexanderem218@nemi.tskoli.is>
2018-01-20 18:45:55 +0000

    Add files via upload

---
+++ verkefni 1.py
@@ -0,0 +1,20 @@
+from bottle import route, run, template
+
+@route('/')
+def index():
+    return \
+		"<a href='/about'>About </a><a href='/bio'>Bio </a><a href='/pic'>Pictures</a>"
+	
+@route('/about')
+def about():
+	return "Hér eru upplýsingar um Steve Jobs"
+
+@route('/bio')
+def bio():
+	return "Hér er Biography frá Steve Jobs"
+
+@route('/pic')
+def pic():
+	return "Hér er mynd af Steve Jobs"
+
+run(host='localhost', port=8080)
